72599860b33fed26dba7a0c16554a7186ba48a09
Tim Johnson <pavementii@gmail.com>
2019-06-14 12:47:35 -0400

    [fc, t] return first repeated number and undefined if none repeated

--- google-interview-first-recurring-char.js
+++ google-interview-first-recurring-char.js
@@ -2,15 +2,27 @@
 // Given an array = [2,5,1,2,3,5,1,2,4]:
 // It should return 2
 
-// Given an array = [2,1,1,2,3,5,1,2,4]:
+// Given an array = [2, 1, 1, 2, 3, 5, 1, 2, 4]:
 // It should return 1
 
-// Given an array = [2,3,4,5]:
+// Given an array = [2, 3, 4, 5]:
 // It should return undefined
 
 var input = [2, 5, 1, 2, 3, 5, 1, 2, 4]
+
 function firstRecurringCharacter (input) {
-  return 2
+  let charMap = {}
+
+  for (let i = 0; i < input.length; i++) {
+    let key = input[i]
+    if (charMap[key]) {
+      return key
+    } else {
+      charMap[key] = true
+    }
+  }
+
+  return undefined
 }
 
 // Bonus... What if we had this:
--- google-interview-first-recurring-character.test.js
+++ google-interview-first-recurring-character.test.js
@@ -2,10 +2,24 @@ const firstRecurringCharacter = require('./google-interview-first-recurring-char
 
 /* global test expect describe */
 
-describe('isBalanced()', () => {
-  test('should return 9', () => {
+describe('firstRecurringCharacter()', () => {
+  test('should return first repeated number', () => {
     var input = [2, 5, 1, 2, 3, 5, 1, 2, 4]
     var solution = 2
+
+    expect(firstRecurringCharacter.firstRecurringCharacter(input)).toEqual(solution)
+  })
+
+  test('should return first repeated number', () => {
+    var input = [2, 1, 1, 2, 3, 5, 1, 2, 4]
+    var solution = 1
+
+    expect(firstRecurringCharacter.firstRecurringCharacter(input)).toEqual(solution)
+  })
+
+  test('should return undefined', () => {
+    var input = [2, 3, 4, 5]
+    var solution
 
     expect(firstRecurringCharacter.firstRecurringCharacter(input)).toEqual(solution)
   })
