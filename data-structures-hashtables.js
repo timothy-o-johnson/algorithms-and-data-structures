f6da615620b680e64128796aae0624312cabdb3d
Tim Johnson <pavementii@gmail.com>
2019-06-14 11:58:37 -0400

    [fc] fix set() bug; refactor get()

--- data-structures-hashtables.js
+++ data-structures-hashtables.js
@@ -15,7 +15,7 @@ class HashTable {
     var hashAddress = this._hash(key)
     if (this.data[hashAddress]) {
       // push new key-value
-      this.data[hashAddress] = this.data[hashAddress].push([key, value])
+      this.data[hashAddress].push([key, value])
     } else {
       // if doesn't exist push new
       this.data[hashAddress] = [[key, value]]
@@ -26,15 +26,13 @@ class HashTable {
     var hashAddress = this._hash(key)
     var hashData = this.data[hashAddress]
     // if hashData exists and there is no other data
-    if (hashData && hashData.length === 1) {
-      return this.data[hashAddress][0][1]
-    } else if (hashData && hashData.length > 1) {
+    if (hashData && hashData.length > 1) {
       // if hashdata is greater than one, loop through the array at that hash and find the key and value
       var hashArray = this.data[hashAddress]
       // loop through hashArray
       for (var i = 0; i < hashArray.length; i++) {
         var hashArrayKey = hashArray[i][0]
-        if (hashArrayKey === key) return hashArray[i]
+        if (hashArrayKey === key) return hashArray[i][1]
       }
     } else {
       return undefined
@@ -42,8 +40,10 @@ class HashTable {
   }
 }
 
-const myHashTable = new HashTable(50)
+const myHashTable = new HashTable(2)
 myHashTable.set('grapes', 10000)
 myHashTable.get('grapes')
 myHashTable.set('apples', 9)
 myHashTable.get('apples')
+myHashTable.set('oranges', 6)
+myHashTable.get('oranges')
