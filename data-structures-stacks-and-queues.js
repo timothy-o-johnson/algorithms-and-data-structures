f88d708530658ef9c5bbc9e6b6a73313a7202739
Tim Johnson <pavementii@gmail.com>
2019-06-22 11:49:47 -0400

    [fc] prepped for export

--- data-structures-stacks-and-queues.js
+++ data-structures-stacks-and-queues.js
@@ -4,110 +4,110 @@
 //   }
 // }
 
-// class Stack {
-//   constructor () {
-//     this.top = null
-//     this.bottom = null
-//     this.length = 0
-//   }
+class Stack {
+  constructor () {
+    this.top = null
+    this.bottom = null
+    this.length = 0
+  }
 
-//   peek () {
-//     if (this.length !== 0) {
-//       console.log(`peek: ${this.top.value}!`)
-//     }
-//     return this.length !== 0 ? this.top.value : console.log('(0) empty stack')
-//   }
+  peek () {
+    if (this.length !== 0) {
+      console.log(`stack.peek: ${this.top.value}!`)
+    }
+    return this.length !== 0 ? this.top.value : console.log('(0) empty stack')
+  }
 
-//   // 5 -- 4 -- 3 -- 2 -- 1
-//   push (value) {
-//     const newNode = new Node(value)
+  // 5 -- 4 -- 3 -- 2 -- 1
+  push (value) {
+    const newNode = new Node(value)
 
-//     if (this.length === 0) {
-//       this.top = newNode
-//       this.bottom = newNode
-//     } else {
-//       newNode.next = this.top
-//       this.top = newNode
-//     }
+    if (this.length === 0) {
+      this.top = newNode
+      this.bottom = newNode
+    } else {
+      newNode.next = this.top
+      this.top = newNode
+    }
 
-//     this.length++
-//     this.printStack()
-//   }
+    this.length++
+   // this.printStack()
+  }
 
-//   pop () {
-//     let popped
+  pop () {
+    let popped
 
-//     if (this.length > 0) {
-//       popped = this.top.value
-//       this.top = this.top.next
-//       this.length--
-//     }
+    if (this.length > 0) {
+      popped = this.top.value
+      this.top = this.top.next
+      this.length--
+    }
 
-//     if (this.length === 0) {
-//       this.top = null
-//       this.bottom = null
-//     }
+    if (this.length === 0) {
+      this.top = null
+      this.bottom = null
+    }
 
-//     this.printStack()
+    // this.printStack()
 
-//     return popped || '(0) stack is empty'
-//   }
+    return popped || 'null'
+  }
 
-//   printStack () {
-//     let currentNode = this.top
-//     let stack = ''
+  printStack () {
+    let currentNode = this.top
+    let stack = ''
 
-//     if (currentNode) {
-//       while (currentNode && currentNode.next !== null) {
-//         stack += ` ${currentNode.value} -->`
-//         currentNode = currentNode.next
-//       }
+    if (currentNode) {
+      while (currentNode && currentNode.next !== null) {
+        stack += ` ${currentNode.value} -->`
+        currentNode = currentNode.next
+      }
 
-//       console.log(`(${this.length}) ${stack} ${currentNode.value}`)
-//     }
-//     console.log(this)
-//   }
+      console.log(`(${this.length}) ${stack} ${currentNode.value}`)
+    }
+    //console.log(this)
+  }
 
-//   isEmpty () {
-//     return !this.length
-//   }
-// }
+  isEmpty () {
+    return !this.length
+  }
+}
 
-// class ArrayStack {
-//   constructor () {
-//     this.stack = []
-//   }
+class ArrayStack {
+  constructor () {
+    this.stack = []
+  }
 
-//   peek () {
-//     if (this.stack.length !== 0) {
-//       console.log(`peek: ${this.stack[this.stack.length - 1]}!`)
-//     }
-//     return this.stack.length !== 0 ? this.stack[this.stack.length - 1] : console.log('(0) empty stack')
-//   }
+  peek () {
+    if (this.stack.length !== 0) {
+      console.log(`peek: ${this.stack[this.stack.length - 1]}!`)
+    }
+    return this.stack.length !== 0 ? this.stack[this.stack.length - 1] : console.log('(0) empty stack')
+  }
 
-//   // 5 -- 4 -- 3 -- 2 -- 1
-//   push (value) {
-//     this.stack.push(value)
+  // 5 -- 4 -- 3 -- 2 -- 1
+  push (value) {
+    this.stack.push(value)
 
-//     this.printStack()
-//   }
+    this.printStack()
+  }
 
-//   pop () {
-//     let popped = this.stack.pop()
+  pop () {
+    let popped = this.stack.pop()
 
-//     this.printStack()
+    this.printStack()
 
-//     return popped || '(0) stack is empty'
-//   }
+    return popped || '(0) stack is empty'
+  }
 
-//   printStack () {
-//     console.log(this.stack)
-//   }
+  printStack () {
+    console.log(this.stack)
+  }
 
-//   isEmpty () {
-//     return !this.stack.length
-//   }
-// }
+  isEmpty () {
+    return !this.stack.length
+  }
+}
 
 // const myStack = new ArrayStack()
 
@@ -146,7 +146,7 @@ class Queue {
 
   /**
    * add an item to the queue from the rear
-   * @param {*} value 
+   * @param {*} value
    */
   enqueue (value) {
     // create new node
@@ -182,7 +182,7 @@ class Queue {
     } else {
       const secondNode = this.first.next
       secondNode.prev = null
-  
+
       this.first = secondNode
 
       this.length--
@@ -220,18 +220,20 @@ class Queue {
     }
   }
 }
-const myQueue = new Queue()
+//const myQueue = new Queue()
 
-myQueue.enqueue(1)
-myQueue.enqueue(2)
-myQueue.enqueue(3)
-myQueue.peek()
-myQueue.enqueue(4)
-myQueue.enqueue(5)
-myQueue.dequeue()
-myQueue.dequeue()
-myQueue.dequeue()
-myQueue.peek()
-myQueue.dequeue()
-myQueue.dequeue()
-myQueue.peek()
+// myQueue.enqueue(1)
+// myQueue.enqueue(2)
+// myQueue.enqueue(3)
+// myQueue.peek()
+// myQueue.enqueue(4)
+// myQueue.enqueue(5)
+// myQueue.dequeue()
+// myQueue.dequeue()
+// myQueue.dequeue()
+// myQueue.peek()
+// myQueue.dequeue()
+// myQueue.dequeue()
+// myQueue.peek()
+
+module.exports = { Stack, Node }
