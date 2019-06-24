2f27c4ea9111772532b66ffcb796dacec085375d
Tim Johnson <pavementii@gmail.com>
2019-06-24 17:47:55 -0400

    [fc] add remove(); fix recursive call in binarySearchLookUp()

--- data-structures-trees.js
+++ data-structures-trees.js
@@ -33,16 +33,17 @@ class BinarySearchTree {
   lookup (value) {
     // confirm that there is a root
     let node = this.root
-    binarySearchLookUp(value, node)
+    let result = binarySearchLookUp(value, node)
 
-    // Code here
+    return result
+
     function binarySearchLookUp (value, node) {
       if (node) {
         if (value === node.value) {
           console.log(`node found: ${value}! \n${JSON.stringify(node)}`)
           return node
         } else {
-          value > node.value ? binarySearchLookUp(value, node.right) : binarySearchLookUp(value, node.left)
+          return value > node.value ? binarySearchLookUp(value, node.right) : binarySearchLookUp(value, node.left)
         }
       } else {
         console.log('value not found')
@@ -50,21 +51,86 @@ class BinarySearchTree {
       }
     }
   }
-  // remove
+
+  remove (value) {
+    // find matching node
+    let nodeToRemove = this.lookup(value)
+
+    // find single leaf on left side
+    let replacementNode = findSingleLeaf(nodeToRemove)
+
+    replacementNode.left = nodeToRemove.left
+    replacementNode.right = nodeToRemove.right
+    // remove reference to node to be removed
+    replaceParentReference(value, this.root, replacementNode)
+
+    nodeToRemove = replacementNode
+
+    // clear the replacement node's parent
+    replaceMatchingNodeWithValue(replacementNode.left, replacementNode.value, null)
+
+    function findSingleLeaf (node) {
+      let leaf = null
+      let currentNode = node
+
+      while (leaf === null) {
+        if ((currentNode.left === null) & (currentNode.right === null)) {
+          leaf = currentNode
+        }
+        currentNode = currentNode.right
+      }
+
+      return leaf
+    }
+
+    function replaceMatchingNodeWithValue (startingNode, matchingValue, value) {
+      let currentNode = startingNode
+
+      while (currentNode.left !== null) {
+        if (currentNode.left.value === matchingValue) {
+          currentNode.left = value
+          return
+        }
+        currentNode = currentNode.left
+      }
+    }
+
+    function replaceParentReference (value, node, replacementNode) {
+      if (node) {
+        if (value === node.left.value) {
+          node.left = replacementNode
+          return node
+        } else if (value === node.right.value) {
+          node.right = replacementNode
+          return node
+        } else {
+          return value > node.value
+            ? replaceParentReference(value, node.right, replacementNode)
+            : replaceParentReference(value, node.left, replacementNode)
+        }
+      } else {
+        console.log('value not found')
+        return false
+      }
+    }
+  }
 }
 
 const tree = new BinarySearchTree()
 tree.insert(9)
 tree.insert(4)
+tree.insert(81)
 tree.insert(6)
 tree.insert(20)
-tree.insert(170)
+tree.insert(90)
 tree.insert(15)
 tree.insert(1)
+tree.insert(12)
 console.log(JSON.stringify(traverse(tree.root)))
-tree.lookup(20)
-tree.lookup(1000)
-console.log(JSON.stringify(tree))
+// tree.lookup(20)
+// tree.lookup(1000)
+tree.remove(81)
+console.log(JSON.stringify(traverse(tree.root)))
 
 //     9
 //  4     20
