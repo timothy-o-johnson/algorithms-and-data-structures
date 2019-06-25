dd0b7cc19805b1e3d28700f8385a7148a9f04623
Tim Johnson <pavementii@gmail.com>
2019-06-25 08:04:01 -0400

    [fc] add fibonacci()s

--- algorithms-recursion.js
+++ algorithms-recursion.js
@@ -17,5 +17,65 @@ function findFactorialIterative (number) {
   return answer
 }
 
-console.log(findFactorialIterative(5))
-console.log(findFactorialRecursive(5))
+// console.log(findFactorialIterative(5))
+// console.log(findFactorialRecursive(5))
+
+// Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+
+// 0, 1, 1, 2, 3, 5 (5), 8 (6), 13 (7), 21 (8), 34, 55, 89, 144 ...
+// the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3
+
+// For example: fibonacciRecursive(6) should return 8
+
+function fibonacciIterative (n) {
+  if (n === 0) {
+    return 0
+  }
+  // code here;
+  let prev1 = 0
+  let prev2 = 1
+  let total = 1
+
+  for (let i = 2; i <= n; i++) {
+    total = prev2 + prev1
+
+    prev1 = prev2
+    prev2 = total
+  }
+
+  return total
+}
+
+console.log(fibonacciIterative(0))
+console.log(fibonacciIterative(1))
+console.log(fibonacciIterative(2))
+console.log(fibonacciIterative(3))
+console.log(fibonacciIterative(4))
+console.log(fibonacciIterative(5))
+console.log(fibonacciIterative(6))
+console.log(fibonacciIterative(7))
+console.log(fibonacciIterative(8))
+console.log(fibonacciIterative(9))
+
+function fibonacciRecursive (n) {
+  if (n === 0) {
+    return 0
+  } else if (n <= 2) {
+    return 1
+  }
+
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2)
+}
+  
+console.log('...recursive...')
+console.log(fibonacciRecursive(8))
+console.log(fibonacciRecursive(0))
+console.log(fibonacciRecursive(1))
+console.log(fibonacciRecursive(2))
+console.log(fibonacciRecursive(3))
+console.log(fibonacciRecursive(4))
+console.log(fibonacciRecursive(5))
+console.log(fibonacciRecursive(6))
+console.log(fibonacciRecursive(7))
+console.log(fibonacciRecursive(8))
+console.log(fibonacciRecursive(9))
