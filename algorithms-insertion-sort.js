bbfb5cb9d67274776acc046fe10b1e628d706ecf
Tim Johnson <pavementii@gmail.com>
2019-06-30 17:04:17 -0400

    [fc] code sketch

--- algorithms-insertion-sort.js
+++ algorithms-insertion-sort.js
@@ -1,11 +1,37 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
 
 function insertionSort (array) {
+  printArray(array)
+  let current, currentIndex, prev, prevIndex, temp, i
   // loop through list
-  // repeat until current value is larger than the element to its left
-  // compare current element with element to its left
-  // if it is smaller than that element swap it
-  
+  for (i = 1; i < array.length; i++) {
+    currentIndex = i
+    current = array[currentIndex]
+    prevIndex = i - 1
+    prev = array[prevIndex]
+
+    // compare current element with element to its left
+    // if it is smaller than that element swap it
+    // repeat until current value is larger than the element to its left
+    while (prev > current) {
+      temp = prev
+      array[prevIndex] = current
+      array[currentIndex] = temp
+
+      // update prev and location of current element
+      prev = array[--prevIndex]
+      currentIndex--
+    }
+    printArray(array)
+  }
+}
+
+function printArray (array) {
+  let print = ''
+  for (let i = 0; i < array.length; i++) {
+    print += array[i] + ' '
+  }
+  console.log(print)
 }
 
 insertionSort(numbers)
