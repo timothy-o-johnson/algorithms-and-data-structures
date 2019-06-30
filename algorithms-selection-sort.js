eed2767041922a82ecc46052ec1db37e8d044d19
Tim Johnson <pavementii@gmail.com>
2019-06-30 12:10:11 -0400

    [fc] initial commit

---
+++ algorithms-selection-sort.js
@@ -0,0 +1,41 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+
+function selectionSort (array) {
+  let smallest = null
+  let smallestIndex = null
+  let current = null
+  let currentIndex = null
+  let temp = null
+
+  for (let i = 0; i < array.length; i++) {
+    // loop through list
+    smallest = array[i]
+    smallestIndex = i
+    for (let j = i + 1; j < array.length; j++) {
+      current = array[j]
+      currentIndex = j
+      // compare smallest with current
+      if (current < smallest) {
+        smallest = current
+        smallestIndex = currentIndex
+      }
+      // if current number is smallest record its value and its index
+    }
+    // at end of the list replace what is at outer loop index with smallest number index
+    temp = array[i]
+    array[i] = smallest
+    array[smallestIndex] = temp
+    printArray(array)
+  }
+  return numbers
+}
+
+function printArray (array) {
+  let print = ''
+  for (let i = 0; i < array.length; i++) {
+    print += array[i] + ' '
+  }
+  console.log(print)
+}
+
+console.log(selectionSort(numbers))
