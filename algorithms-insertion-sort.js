3143994d735227db4049385417fb03e104e94c78
Tim Johnson <pavementii@gmail.com>
2019-06-30 12:53:58 -0400

    [fc] comment algorithm

---
+++ algorithms-insertion-sort.js
@@ -0,0 +1,12 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+
+function insertionSort (array) {
+  // loop through list
+  // repeat until current value is larger than the element to its left
+  // compare current element with element to its left
+  // if it is smaller than that element swap it
+  
+}
+
+insertionSort(numbers)
+console.log(numbers)
