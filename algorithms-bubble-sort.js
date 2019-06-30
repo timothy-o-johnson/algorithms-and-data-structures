3d2c8e700536a174abbb96e8dcafa5819bed14f9
Tim Johnson <pavementii@gmail.com>
2019-06-30 09:49:39 -0400

    [sx] wrote algorithm

---
+++ algorithms-bubble-sort.js
@@ -0,0 +1,14 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+
+function bubbleSort (array) {
+// for each element in the array (if there's a next element-- or until we reach the last element)
+// take the current element and the next element
+// if the first element is larger than the second, switch them (we can switch because we are not changing the length)
+// increment switch count
+// otherwise go the next element
+// do while switch count is greater than zero
+
+}
+
+bubbleSort(numbers)
+console.log(numbers)
