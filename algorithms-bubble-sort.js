1c09192422de52044c1625e5c4997cf823cd3c95
Tim Johnson <pavementii@gmail.com>
2019-06-30 12:09:54 -0400

    [fc] add code

--- algorithms-bubble-sort.js
+++ algorithms-bubble-sort.js
@@ -1,13 +1,39 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
 
 function bubbleSort (array) {
-// for each element in the array (if there's a next element-- or until we reach the last element)
-// take the current element and the next element
-// if the first element is larger than the second, switch them (we can switch because we are not changing the length)
-// increment switch count
-// otherwise go the next element
-// do while switch count is greater than zero
+  let switches = 1
+  // for each element in the array (if there's a next element-- or until we reach the last element)
+  let first, second
 
+  while (switches) {
+    // reset switches
+    switches = 0
+    for (let i = 0; i < array.length; i++) {
+      // take the current element and the next element
+      first = array[i]
+      second = array[i + 1]
+
+      // if the first element is larger than the second, switch them (we can switch because we are not changing the length)
+      if (first > second) {
+        array[i + 1] = first
+        array[i] = second
+        // increment switch count
+        switches++
+        printArray(array)
+      }
+      // otherwise go the next element
+
+      // do while switch count is greater than zero
+    }
+  }
+}
+
+function printArray (array) {
+  let print = ''
+  for (let i = 0; i < array.length; i++) {
+    print += array[i] + ' '
+  }
+  console.log(print)
 }
 
 bubbleSort(numbers)
