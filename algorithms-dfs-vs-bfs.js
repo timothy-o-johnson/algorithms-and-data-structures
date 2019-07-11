d1cdc807cb5b29191c6181c9ba40200545fb7bfb
Tim Johnson <pavementii@gmail.com>
2019-07-11 19:20:19 -0400

    [fc] create new validation function bc BFS didn't pass the tests

--- algorithms-dfs-vs-bfs.js
+++ algorithms-dfs-vs-bfs.js
@@ -1,9 +1,8 @@
 //If you know a solution is not far from the root of the tree:
 //breadth
 
-//If the tree is very deep and solutions are rare: 
+//If the tree is very deep and solutions are rare:
 // breadth-- although memory intensive; depth will take a long time since we use recursion
-
 
 //If the tree is very wide:
 // depth first (breadth will use a lot of memory)
@@ -17,258 +16,302 @@
 //Finding the shortest path:
 // breadth (that's the point)
 class Node {
-    constructor(value) {
-        this.left = null;
-        this.right = null;
-        this.value = value;
-    }
+  constructor(value) {
+    this.left = null
+    this.right = null
+    this.value = value
+  }
 }
 
 class BinarySearchTree {
-    constructor() {
-        this.root = null;
-    }
-    insert(value) {
-        const newNode = new Node(value);
-        if (this.root === null) {
-            this.root = newNode;
+  constructor() {
+    this.root = null
+  }
+  insert(value) {
+    const newNode = new Node(value)
+    if (this.root === null) {
+      this.root = newNode
+    } else {
+      let currentNode = this.root
+      while (true) {
+        if (value < currentNode.value) {
+          //Left
+          if (!currentNode.left) {
+            currentNode.left = newNode
+            return this
+          }
+          currentNode = currentNode.left
         } else {
-            let currentNode = this.root;
-            while (true) {
-                if (value < currentNode.value) {
-                    //Left
-                    if (!currentNode.left) {
-                        currentNode.left = newNode;
-                        return this;
-                    }
-                    currentNode = currentNode.left;
-                } else {
-                    //Right
-                    if (!currentNode.right) {
-                        currentNode.right = newNode;
-                        return this;
-                    }
-                    currentNode = currentNode.right;
-                }
-            }
+          //Right
+          if (!currentNode.right) {
+            currentNode.right = newNode
+            return this
+          }
+          currentNode = currentNode.right
         }
+      }
     }
-    lookup(value) {
-        if (!this.root) {
-            return false;
-        }
-        let currentNode = this.root;
-        while (currentNode) {
-            if (value < currentNode.value) {
-                currentNode = currentNode.left;
-            } else if (value > currentNode.value) {
-                currentNode = currentNode.right;
-            } else if (currentNode.value === value) {
-                return currentNode;
-            }
-        }
-        return null
+  }
+  lookup(value) {
+    if (!this.root) {
+      return false
     }
-    remove(value) {
-        if (!this.root) {
-            return false;
-        }
-        let currentNode = this.root;
-        let parentNode = null;
-        while (currentNode) {
-            if (value < currentNode.value) {
-                parentNode = currentNode;
-                currentNode = currentNode.left;
-            } else if (value > currentNode.value) {
-                parentNode = currentNode;
-                currentNode = currentNode.right;
-            } else if (currentNode.value === value) {
-                //We have a match, get to work!
-
-                //Option 1: No right child: 
-                if (currentNode.right === null) {
-                    if (parentNode === null) {
-                        this.root = currentNode.left;
-                    } else {
-
-                        //if parent > current value, make current left child a child of parent
-                        if (currentNode.value < parentNode.value) {
-                            parentNode.left = currentNode.left;
-
-                            //if parent < current value, make left child a right child of parent
-                        } else if (currentNode.value > parentNode.value) {
-                            parentNode.right = currentNode.left;
-                        }
-                    }
-
-                    //Option 2: Right child which doesnt have a left child
-                } else if (currentNode.right.left === null) {
-                    if (parentNode === null) {
-                        this.root = currentNode.left;
-                    } else {
-                        currentNode.right.left = currentNode.left;
-
-                        //if parent > current, make right child of the left the parent
-                        if (currentNode.value < parentNode.value) {
-                            parentNode.left = currentNode.right;
-
-                            //if parent < current, make right child a right child of the parent
-                        } else if (currentNode.value > parentNode.value) {
-                            parentNode.right = currentNode.right;
-                        }
-                    }
-
-                    //Option 3: Right child that has a left child
-                } else {
-
-                    //find the Right child's left most child
-                    let leftmost = currentNode.right.left;
-                    let leftmostParent = currentNode.right;
-                    while (leftmost.left !== null) {
-                        leftmostParent = leftmost;
-                        leftmost = leftmost.left;
-                    }
-
-                    //Parent's left subtree is now leftmost's right subtree
-                    leftmostParent.left = leftmost.right;
-                    leftmost.left = currentNode.left;
-                    leftmost.right = currentNode.right;
-
-                    if (parentNode === null) {
-                        this.root = leftmost;
-                    } else {
-                        if (currentNode.value < parentNode.value) {
-                            parentNode.left = leftmost;
-                        } else if (currentNode.value > parentNode.value) {
-                            parentNode.right = leftmost;
-                        }
-                    }
-                }
-                return true;
-            }
-        }
+    let currentNode = this.root
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right
+      } else if (currentNode.value === value) {
+        return currentNode
+      }
     }
-
-    BreadthFirstSearch() {
-        let currentNode = this.root;
-        let list = []; // search results
-        let queue = []; // pointers to nodes that need to be checked
-        queue.push(currentNode);
-
-        while (queue.length > 0) {
-            currentNode = queue.shift();
-            list.push(currentNode.value);
-            if (currentNode.left) {
-                queue.push(currentNode.left);
-            }
-            if (currentNode.right) {
-                queue.push(currentNode.right);
-            }
-        }
-        return list;
+    return null
+  }
+  remove(value) {
+    if (!this.root) {
+      return false
     }
+    let currentNode = this.root
+    let parentNode = null
+    while (currentNode) {
+      if (value < currentNode.value) {
+        parentNode = currentNode
+        currentNode = currentNode.left
+      } else if (value > currentNode.value) {
+        parentNode = currentNode
+        currentNode = currentNode.right
+      } else if (currentNode.value === value) {
+        //We have a match, get to work!
 
-    BreadthFirstSearchR(queue, list) {
-        if (!queue.length) {
-            return list;
-        }
-        const currentNode = queue.shift();
-        list.push(currentNode.value);
+        //Option 1: No right child:
+        if (currentNode.right === null) {
+          if (parentNode === null) {
+            this.root = currentNode.left
+          } else {
+            //if parent > current value, make current left child a child of parent
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.left
 
-        if (currentNode.left) {
-            queue.push(currentNode.left);
-        }
-        if (currentNode.right) {
-            queue.push(currentNode.right);
-        }
+              //if parent < current value, make left child a right child of parent
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.left
+            }
+          }
 
-        return this.BreadthFirstSearchR(queue, list);
+          //Option 2: Right child which doesnt have a left child
+        } else if (currentNode.right.left === null) {
+          if (parentNode === null) {
+            this.root = currentNode.left
+          } else {
+            currentNode.right.left = currentNode.left
+
+            //if parent > current, make right child of the left the parent
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.right
+
+              //if parent < current, make right child a right child of the parent
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.right
+            }
+          }
+
+          //Option 3: Right child that has a left child
+        } else {
+          //find the Right child's left most child
+          let leftmost = currentNode.right.left
+          let leftmostParent = currentNode.right
+          while (leftmost.left !== null) {
+            leftmostParent = leftmost
+            leftmost = leftmost.left
+          }
+
+          //Parent's left subtree is now leftmost's right subtree
+          leftmostParent.left = leftmost.right
+          leftmost.left = currentNode.left
+          leftmost.right = currentNode.right
+
+          if (parentNode === null) {
+            this.root = leftmost
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = leftmost
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = leftmost
+            }
+          }
+        }
+        return true
+      }
+    }
+  }
+
+  BreadthFirstSearch() {
+    let currentNode = this.root
+    let list = [] // search results
+    let queue = [] // pointers to nodes that need to be checked
+    queue.push(currentNode)
+
+    while (queue.length > 0) {
+      currentNode = queue.shift()
+      list.push(currentNode.value)
+      if (currentNode.left) {
+        queue.push(currentNode.left)
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right)
+      }
+    }
+    return list
+  }
+
+  BreadthFirstSearchR(queue, list) {
+    if (!queue.length) {
+      return list
+    }
+    const currentNode = queue.shift()
+    list.push(currentNode.value)
+
+    if (currentNode.left) {
+      queue.push(currentNode.left)
+    }
+    if (currentNode.right) {
+      queue.push(currentNode.right)
     }
 
-    //      9
-    //   4     20
-    // 1  6  15  170
+    return this.BreadthFirstSearchR(queue, list)
+  }
 
-    traverseInOrder(list, currentNode) {
+  //      9
+  //   4      20
+  // 1   6  15  170
 
-        if (currentNode.left !== null) {
-            this.traverseInOrder(list, currentNode.left)
-        }
+  traverseInOrder(list, currentNode) {
+    // get down to smallest node add that node
+    if (currentNode.left !== null) {
+      this.traverseInOrder(list, currentNode.left)
+    }
 
+    // add itself
+    list.push(currentNode.value)
+
+    // add the node to right
+    if (currentNode.right !== null) {
+      this.traverseInOrder(list, currentNode.right)
+    }
+    return list
+  }
+
+  traversePreOrder(list, currentNode) {
+    list.push(currentNode.value)
+
+    if (currentNode.left !== null) {
+      this.traversePreOrder(list, currentNode.left)
+    }
+
+    if (currentNode.right !== null) {
+      this.traversePreOrder(list, currentNode.right)
+    }
+    return list
+  }
+
+  traversePostOrder(list, currentNode) {
+    if (currentNode.left !== null) {
+      this.traversePostOrder(list, currentNode.left)
+    }
+
+    if (currentNode.right !== null) {
+      this.traversePostOrder(list, currentNode.right)
+    }
+
+    list.push(currentNode.value)
+    return list
+  }
+
+  validateDFS(list, currentNode) {
+    return modifiedTraverseInOrder(list, currentNode)[0] === -1 ? false : true
+
+    function modifiedTraverseInOrder(list, currentNode) {
+      if (list[0] === -1) {
+        list = -1
+        return -1
+      }
+      // go down to the smallest node on the left
+      if (currentNode.left !== null) {
+        modifiedTraverseInOrder(list, currentNode.left)
+      }
+
+      // add current node
+      // if list first element is -1, return -1
+      if (list[0] === -1) {
+        list = -1
+        return -1
+      }
+      // else if currentNode.value is greater than or equal to the last element, add element
+      else if (list.length === 0 || currentNode.value >= list[list.length - 1]) {
         list.push(currentNode.value)
+      } else {
+        // else replace list with [-1]
+        list[0] = -1
 
-        if (currentNode.right !== null) {
-            this.traverseInOrder(list, currentNode.right)
-        }
-        //console.log(JSON.stringify(list))
-        return list
+        // return list
+      }
+
+      // add smallest node on the right
+      if (currentNode.right !== null) {
+        modifiedTraverseInOrder(list, currentNode.right)
+      }
+      //console.log(JSON.stringify(list))
+      return list
+    }
+  }
+  validateBFS(queue, list) {
+    if (!queue.length) {
+      return list
+    }
+    const currentNode = queue.shift()
+
+    list.push(currentNode.value)
+    const left = currentNode.left
+    const right = currentNode.right
+    const leftValue = left ? left.value : null
+    const rightValue = right ? right.value : null
+    const leftIsGreaterThanCurrentNode = currentNode.value < leftValue && left !== null
+    const rightIsLessThanCurrentNode = currentNode.value > rightValue && right !== null
+
+    if (rightIsLessThanCurrentNode || leftIsGreaterThanCurrentNode || currentNode.value === null) {
+      console.log("not valid")
+      return -1
     }
 
-    traversePreOrder(list, currentNode) {
-        list.push(currentNode.value)
-
-        if (currentNode.left !== null) {
-            this.traversePreOrder(list, currentNode.left)
-        }
-
-        if (currentNode.right !== null) {
-            this.traversePreOrder(list, currentNode.right)
-        }
-        return list
+    if (left) {
+      queue.push(left)
+    }
+    if (right) {
+      queue.push(right)
     }
 
-    traversePostOrder(list, currentNode) {
+    return this.validateBFS(queue, list)
+  }
 
+  printTree() {
+    console.log(JSON.stringify(this.root))
+  }
 
-        if (currentNode.left !== null) {
-            this.traversePostOrder(list, currentNode.left)
-        }
-
-        if (currentNode.right !== null) {
-            this.traversePostOrder(list, currentNode.right)
-        }
-
-        list.push(currentNode.value)
-        return list
+  isValidBST(root, upperBound = Infinity, lowerBound = -Infinity) {
+    if (!root) {
+      return true
     }
+    if (root.val >= upperBound || root.val <= lowerBound) return false
 
-    validateBFS(queue, list) {
-        if (!queue.length) {
-            return list;
-        }
-        const currentNode = queue.shift();
-
-        list.push(currentNode.value);
-        const left = currentNode.left
-        const right = currentNode.right
-        const leftValue = left ? left.value : null
-        const rightValue = right ? right.value : null 
-        const leftIsGreaterThanCurrentNode = currentNode.value < leftValue && left !== null
-        const rightIsLessThanCurrentNode = currentNode.value > rightValue && right !== null
-
-        if (rightIsLessThanCurrentNode || leftIsGreaterThanCurrentNode || currentNode.value === null) {
-            console.log('not valid')
-            return -1
-        }
-
-        if (left) {
-            queue.push(left);
-        }
-        if (right) {
-            queue.push(right);
-        }
-
-        return this.validateBFS(queue, list);
-    }
-
-    printTree(){
-        console.log(JSON.stringify(this.root))
-
+    return (
+      this.isValidBST(root.left, Math.min(upperBound, root.val), lowerBound) &&
+      this.isValidBST(root.right, upperBound, Math.max(lowerBound, root.val))
+    )
+  }
 }
 
-}
-
-const tree = new BinarySearchTree();
+const tree = new BinarySearchTree()
 tree.insert(9)
 tree.insert(4)
 tree.insert(6)
@@ -289,34 +332,32 @@ tree.printTree()
 
 //     9
 //  4     20
-//1  6  15  170
+//1  10  15  170
 
 var node = {
-    left: {
-      left: { left: null, right: null, value: 1 },
-      right: { left: null, right: null, value: 10 },
-      value: 4
-    },
-    right: {
-      left: { left: null, right: null, value: 15 },
-      right: { left: null, right: null, value: 170 },
-      value: 20
-    },
-    value: 9
-  }
+  left: {
+    left: { left: null, right: null, value: 1 },
+    right: { left: null, right: null, value: 10 },
+    value: 4
+  },
+  right: {
+    left: { left: null, right: null, value: 15 },
+    right: { left: null, right: null, value: 170 },
+    value: 20
+  },
+  value: 9
+}
 
 tree.root = node
 
 console.log(tree.validateBFS([tree.root], []))
+console.log(tree.isValidBST(tree.root))
+console.log(tree.traverseInOrder([], tree.root))
+console.log(tree.validateDFS([], tree.root))
 
 function traverse(node) {
-    const tree = { value: node.value };
-    tree.left = node.left === null ? null : traverse(node.left);
-    tree.right = node.right === null ? null : traverse(node.right);
-    return tree;
+  const tree = { value: node.value }
+  tree.left = node.left === null ? null : traverse(node.left)
+  tree.right = node.right === null ? null : traverse(node.right)
+  return tree
 }
-
-
-
-
-
